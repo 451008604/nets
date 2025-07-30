baedeb9fa7206d40fa47d0b8373bbe71b438a7a3
Mr.Eric <451008604@qq.com>
2025-07-30 16:43:08 +0800

    【优化】测试脚本

--- test/client_kcp.go
+++ test/client_kcp.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"github.com/451008604/nets/network"
 	pb "github.com/451008604/nets/proto/bin"
-	"github.com/gorilla/websocket"
+	"github.com/xtaci/kcp-go"
+	"net"
 	"time"
 )
 
@@ -18,20 +19,19 @@ func main() {
 	data := network.NewDataPack().Pack(network.NewMsgPackage(int32(pb.MsgId_Echo), msg))
 
 	sendWebSocketMessage := func(data []byte) {
-		conn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:17002", nil)
+		conn, err := kcp.Dial("127.0.0.1:17004")
 		if err != nil {
 			println(err.Error())
 			return
 		}
 		defer conn.Close()
 
-		go func(c *websocket.Conn) {
+		go func(c net.Conn) {
 			for {
-				if _, message, e := c.ReadMessage(); e == nil {
-					if len(message) != 0 {
-						pack := network.NewDataPack().UnPack(message)
-						fmt.Printf("服务器：%v - %s\n", pack.GetMsgId(), pack.GetData())
-					}
+				buf := make([]byte, 4096)
+				if message, e := c.Read(buf); e == nil {
+					pack := network.NewDataPack().UnPack(buf[:message])
+					fmt.Printf("服务器：%v - %s\n", pack.GetMsgId(), pack.GetData())
 				} else {
 					break
 				}
@@ -39,7 +39,11 @@ func main() {
 		}(conn)
 
 		// 发送消息
-		_ = conn.WriteMessage(websocket.BinaryMessage, append(append(append(append(data, data...), data...), data...), data...))
+		_, _ = conn.Write(append(append(append(append(data, data...), data...), data...), data...))
+		// _, _ = conn.Write(append(data, data...))
+		// _, _ = conn.Write(append(data, data...))
+		// _, _ = conn.Write(append(data, data...))
+		// _, _ = conn.Write(append(data, data...))
 
 		// go func(c *websocket.Conn) {
 		// 	time.Sleep(time.Second * time.Duration(3+rand.Intn(2)))
