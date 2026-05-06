4378689fb9ea87a2bddeaa36e775544514768fb6
451008604 <451008604@qq.com>
2026-05-06 20:21:04 +0800

    test: update Docker test configuration and simplify startup script steps
    - Increase client replicas from 4 to 20 for higher load testing
    
    - Simplify step numbering from 4 steps to 3 steps in start.sh
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- test/start.sh
+++ test/start.sh
@@ -16,15 +16,15 @@ echo "[Cleanup]"
 docker compose down --remove-orphans
 echo "Done!"
 echo ""
-echo "[1/4] Compile..."
+echo "[1/3] Compile..."
 GOOS=linux GOARCH=amd64 go build -o "$SERVER_BIN" "$SERVER_DIR/server.go"
 GOOS=linux GOARCH=amd64 go build -o "$CLIENT_BIN" "$CLIENT_DIR/client.go"
 
 echo ""
-echo "[2/4] Build Docker images..."
+echo "[2/3] Build Docker images..."
 cd "$SCRIPT_DIR"
 docker compose build
 
 echo ""
-echo "[4/4] Run test..."
+echo "[3/3] Run test..."
 docker compose up
